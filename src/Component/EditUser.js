3e1a615bb906d1bca49daa86e44eb8bb658d13bb
Lata Chapadia <latarana4u@gmail.com>
2023-09-01 22:21:37 +0530

    try to edit user

--- src/Component/EditUser.js
+++ src/Component/EditUser.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import { Navigate,Link } from 'react-router-dom'
-
+import {connect } from 'react-redux'
+import { userInfo } from '../redux/allAction'
 export class EditUser extends Component {
     constructor(props){
         super(props)
         this.state={
-            name:'',
+            uname:'',
             email:'',
             phone:'',
             redirect:false
         }
     }
-    componentDidMount(){}
+    componentDidMount(){
+      this.props.getUserDetail(this.props.match.params.id)
+       console.log(this.props)
+    }
     componentDidUpdate(){}
     inputHandle=(e)=>{
         this.setState({[e.target.name]:e.target.value})
@@ -23,36 +27,103 @@ export class EditUser extends Component {
         return <Navigate to="/home" />
     }
     return (
-        <div className='container'>
-        <div className='row login'>
-            <h3 className='heading' style={{marginTop:'20px'}}>Add Registation</h3>
-            <div className='col-md-2'></div>
-            <div className='col-md-6'>
-                <form autoComplete='off' action='/login-user' method='post'>
-                <div className='col-md-3'>
-                    <label className='form-label'>User Name</label>
-                    <input type='text' className='form-control' name='name' value={uname} onChange={this.inputHandle} />
-                 </div>
-                 <div className='col-md-3'>
-                    <label className='form-label'> Email</label>
-                    <input type='text' autoComplete='off' className='form-control' name='email' value={email} onChange={this.inputHandle} />
-                 </div>
-                 <div className='col-md-3'>
-                    <label className='form-label'>Phone</label>
-                    <input type='text' autoComplete='off' className='form-control' name='phone' maxLength="10" value={phone} onChange={this.inputHandle} />
-                 </div>
-                 <div>
-                    <button type="button" className='btn btn-primary' onClick={this.submitButton}>Edit User</button>
-                    <Link to="/home"><button type='button' id='addUserBtn' className='btn btn-success'>Back</button></Link>
-                 </div>
-                </form>
-            </div>
+       <>
+      <section
+        className="vh-100 bg-image"
+        style={{
+          backgroundImage: `url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')`,
+        }}
+      >
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card" style={{ borderRadius: `15px` }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">Change User Info</h2>
+                    <form autoComplete="off" action="/login-user" method="post">
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="uname"
+                          value={uname}
+                          onChange={this.inputHandle}
+                        />
+                        <label className="form-label" htmlFor="form3Example1cg">
+                        Name
+                        </label>
+                      </div>
 
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          id="form3Example3cg"
+                          autoComplete="off"
+                          className="form-control"
+                          name="email"
+                          value={email}
+                          onChange={this.inputHandle}
+                        />
+                        <label className="form-label" htmlFor="form3Example3cg">
+                          {" "}
+                          Email
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          autoComplete="off"
+                          className="form-control"
+                          name="phone"
+                          maxLength="10"
+                          value={phone}
+                          onChange={this.inputHandle}
+                        />
+                        <label className="form-label">Phone</label>
+                      </div>
+
+                      <div className="d-flex justify-content-center">
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={this.submitButton}
+                        >
+                          Add User
+                        </button> &nbsp;
+                        <Link to="/home">
+                          <button
+                            type="button"
+                            id="addUserBtn"
+                            className="btn btn-success"
+                          >
+                            Back
+                          </button>
+                        </Link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </section>
+    </>
 
     )
   }
 }
+const mapStateToProps=(state)=>{
+    return{
+        myUsers:state.user
+    }
+}
 
-export default EditUser
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    getUserDetail:(id)=>{dispatch(userInfo(id))}
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(EditUser)
--- src/Component/Registration.js
+++ src/Component/Registration.js
@@ -1,58 +1,116 @@
-import React, { useState } from 'react'
-import {Link,useNavigate} from 'react-router-dom';
-import shortid from 'shortid';
-import { userRegister } from '../redux/allAction';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import shortid from "shortid";
+import { userRegister } from "../redux/allAction";
+import { useDispatch } from "react-redux";
 
 const Registration = () => {
-    const dispatch = useDispatch()
-    const history = useNavigate()
-    const [inputField,setInputField]= useState({
-        name:'',
-        email:'',
-        phone:'',
-    });
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const [inputField, setInputField] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
 
-    const inputHandle = (e)=>{
-        setInputField({...inputField,[e.target.name]:e.target.value});
-    }
-    
-    const submitButton=async()=>{
-        Object.assign({id:shortid.generate()},inputField)
-        // console.log(inputField) //to check data with id
-        dispatch(userRegister(inputField))//to add in the state
-        history('/') //to redirect homepage
+  const inputHandle = (e) => {
+    setInputField({ ...inputField, [e.target.name]: e.target.value });
+  };
 
-    }
+  const submitButton = async () => {
+    Object.assign({ id: shortid.generate() }, inputField);
+    // console.log(inputField) //to check data with id
+    dispatch(userRegister(inputField)); //to add in the state
+    history("/"); //to redirect homepage
+  };
   return (
-    <div className='container'>
-        <div className='row login'>
-            <h3 className='heading' style={{marginTop:'20px'}}>Add Registation</h3>
-            <div className='col-md-2'></div>
-            <div className='col-md-6'>
-                <form autoComplete='off' action='/login-user' method='post'>
-                <div className='mb-3'>
-                    <label className='form-label'>User Name</label>
-                    <input type='text' className='form-control' name='name' value={inputField.name} onChange={inputHandle} />
-                 </div>
-                 <div className='mb-3'>
-                    <label className='form-label'> Email</label>
-                    <input type='text' autoComplete='off' className='form-control' name='email' value={inputField.email} onChange={inputHandle} />
-                 </div>
-                 <div className='mb-3'>
-                    <label className='form-label'>Phone</label>
-                    <input type='text' autoComplete='off' className='form-control' name='phone' maxLength="10" value={inputField.phone} onChange={inputHandle} />
-                 </div>
-                 <div>
-                    <button type="button" className='btn btn-primary' onClick={submitButton}>Add User</button>
-                    <Link to="/home"><button type='button' id='addUserBtn' className='btn btn-success'>Back</button></Link>
-                 </div>
-                </form>
+    <>
+      <section
+        className="vh-100 bg-image"
+        style={{
+          backgroundImage: `url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')`,
+        }}
+      >
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card" style={{ borderRadius: `15px` }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">
+                      Add User Info
+                    </h2>
+                    <form autoComplete="off" action="/login-user" method="post">
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="name"
+                          value={inputField.name}
+                          onChange={inputHandle}
+                        />
+                        <label className="form-label" htmlFor="form3Example1cg">
+                          User Name
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          id="form3Example3cg"
+                          autoComplete="off"
+                          className="form-control"
+                          name="email"
+                          value={inputField.email}
+                          onChange={inputHandle}
+                        />
+                        <label className="form-label" htmlFor="form3Example3cg">
+                          {" "}
+                          Email
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          autoComplete="off"
+                          className="form-control"
+                          name="phone"
+                          maxLength="10"
+                          value={inputField.phone}
+                          onChange={inputHandle}
+                        />
+                        <label className="form-label">Phone</label>
+                      </div>
+
+                      <div className="d-flex justify-content-center">
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={submitButton}
+                        >
+                          Add User
+                        </button> &nbsp;
+                        <Link to="/home">
+                          <button
+                            type="button"
+                            id="addUserBtn"
+                            className="btn btn-success"
+                          >
+                            Back
+                          </button>
+                        </Link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-
+          </div>
         </div>
-    </div>
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default Registration
+export default Registration;
